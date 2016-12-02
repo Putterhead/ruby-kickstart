f16c0ad7a21152b38b67bac339fb412993931b24
PeteGH <peter@grant-hay.eu>
2016-12-02 22:51:26 +0100

    adding challenge3, s2

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -5,7 +5,23 @@
 # "abcdefg".every_other_char  # => "aceg"
 # "".every_other_char         # => ""
 
+# This is my solution with which I'm being returned the error,
+# NoMethodError:
+       # undefined method `each_index' for "":String
+# class String
+#   def every_other_char
+#     string = ''
+#     string.map(string.each_index.select { |i| i.odd?})
+#     string 
+#   end
+# end
+
 class String
   def every_other_char
+    to_return = ''
+    each_char.with_index do |char, index|
+      to_return << char if index.even?
+    end
+    to_return
   end
 end
