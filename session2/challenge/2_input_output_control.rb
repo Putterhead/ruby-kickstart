79946dc95cc4f8f04aebcf2b0d524de876627220
PeteGH <peter@grant-hay.eu>
2016-12-02 20:18:29 +0100

    adding challenge2, s2

--- session2/challenge/2_input_output_control.rb
+++ session2/challenge/2_input_output_control.rb
@@ -17,11 +17,31 @@
 
 # remember you can try your program out with              $ ruby 2_input_output_control.rb
 # and when you think it is correct, you can test it with  $ rake 2:2
+# This was my attempt at the solution,
+# def hi_hi_goodbye
+#   puts "Enter a number"
+#   number = gets.chomp
+#   print "hi " x number
+  
+#   # your code here
+# end
 
-def hi_hi_goodbye
-  # your code here
+# Here's the solution, though I'm not sure having a method 'prompt' is really necessary
+# instead of 'line' you could just have the variable 'number' (from my solution)
+def prompt
+  puts 'Enter a number or bye'
 end
 
+def hi_hi_goodbye
+  prompt # the method prompt is called, which displays the string 'Enter a number or bye'
+  while (line = gets) && (line !~ /bye/) # first is an assignment statement that returns a line or nil, and is thus boolean
+    # so if you enter in a number other than bye it will carry out the rest of the block
+    line.to_i.times { print 'hi ' } # returning 'line' as an integer x amount of times
+    puts        # it then returns the puts 'Enter a number or bye' again
+    prompt
+  end
+  puts "Goodbye!" # if you enter bye it will return "Goodbye!"
+end
 
 
 
