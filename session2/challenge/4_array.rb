02e01460249d8df901d71bf93ed268104d3e79bc
PeteGH <peter@grant-hay.eu>
2016-12-03 11:01:30 +0100

    adding challenge4, S2

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,25 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+# So, I'm thinking of mulitplying each element by itself and returning
+# it to another array and comparing this to the original array - any 
+# values that correspond can be kept in the array.
+# def get_squares(array)
+#   array.map { |i| i * i} << array2
+#   # I now need part of the block to evaluate the new array for unique values
+#   # and then throw them out and just return the values in common.
+#   # unfortunately, << array2 is no being recognised 'undefined variable'
+#   array.uniq.sort == array2.uniq.sort 
+# end
+
+# This is the solution, which now seems so logical, the array 'numbers' is
+# being checked for values that include squared values (n*n) of the value
+# at each index (|n|), which is then being sorted. 
+# .select is a great way of doing it as it 'returns a new array containing
+# all elements of ary for which the given block returns a true value.
+def get_squares(numbers)
+  numbers.select { |n| numbers.include? n*n }.sort
+end
+# def get_squares(array)
+#   array.inject[:*]
+# end
