3970f7f376aa8c2b44b08c0719d77bcfd4438ee7
PeteGH <peter@grant-hay.eu>
2016-12-03 12:29:49 +0100

    adding challeng6, S2

--- session2/challenge/6_array.rb
+++ session2/challenge/6_array.rb
@@ -14,3 +14,39 @@
 # prime_chars? ['ab', 'cd']       # => false
 # prime_chars? ['a', 'bcd']       # => false
 # prime_chars? ['a', 'b', 'cd']   # => false
+
+# prime number is a number divisible by one and itself, it 
+# must also be a whole number greater than 1.
+
+# Something like this; I don't expect this to run but
+# this is where my thinking is coming from. I still need
+# to figure out how to get the number of elements
+# def prime_chars?(string)
+#   string.length % string.length == 1 && string.length % 1 == 0 ? true : false
+# end
+
+# Solution: Ok, so I didnt think of creating an Integer class
+# but seeing this used helps me understand the use of 
+# classes. First, the method 'prime?' is created, telling
+# Ruby that if 'self' (string) is < 2 its false. I had to look
+# up what .upto was and its a public instance method that iterates
+# the given block, passing in integer values from int (2) up to 
+# and including limit (self) and for those numbers, it returns false
+# if that number is divisibile by itself without a remainder, otherwise
+# it returns true.
+  class Integer
+  def prime?
+    return false if self < 2
+    2.upto Math.sqrt(self) do |i|   # according to Rubydocs sqrt returns 
+      return false if self % i == 0 # the non-negative square root of x.
+    end                             # not sure if I understand this fully
+    true
+  end
+end
+#  Here, its taking the variable 'strings', joining the strings together
+# evaluating then the length, then the method prime? (above) is being 
+# called, which results in true or false.
+def prime_chars?(strings)
+  strings.join.length.prime?
+end
+
