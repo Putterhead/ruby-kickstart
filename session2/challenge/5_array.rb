fb1da85c2beb63a106863101f7a21dc89f59a3e1
PeteGH <peter@grant-hay.eu>
2016-12-03 11:26:54 +0100

    adding challenge 5, S2

--- session2/challenge/5_array.rb
+++ session2/challenge/5_array.rb
@@ -13,3 +13,18 @@
 # mod_three [7]  # => [1]
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
+# So something like if number % 3 == 0 ? exclude : number % 3
+# With numbers.map { |n| numbers.include? n % 3 == 0 } it just
+# returns an array with 'false' for each value given
+# def mod_three(numbers)
+#   numbers.map { |n| numbers n % 3 == 0 }
+# end
+
+# Here's the solution and I hadn't thought of using !=. Its clear
+# to me now that .select is returning a new array with the numbers
+# that to not return true when evaluated for 'number % 3 !=0' and 
+# then using .map a new array is again return with the remainder 
+# value using the block '|number| number % 3'
+def mod_three(numbers)
+  numbers.select { |number| number % 3 != 0 }.map { |number| number % 3 }
+end
