e626322d3706bbead8902c401559a91024baea32
PeteGH <peter@grant-hay.eu>
2016-12-04 10:16:25 +0100

    adding challenge10, S3

--- session3/challenge/10_hashes.rb
+++ session3/challenge/10_hashes.rb
@@ -29,5 +29,44 @@
 # create it from scratch :)
 
 
-def pathify
+def pathify(paths=Hash.new)
+  return paths.map { |path| '/' + path } if paths.is_a? Array 
+
+  to_return = [] # recursive step
+  paths.each do |parent_path, child_dirs|
+    parent_path = '/' + parent_path     # paths begin with a /
+    child_paths = pathify child_dirs    # convert child directories to paths 
+    child_paths.each do |child_path|    # join each child path to it's parent path
+      to_return << (parent_path + child_path)
+    end
+  end
+  to_return
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
